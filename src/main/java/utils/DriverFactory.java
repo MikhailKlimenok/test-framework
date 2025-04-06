47041939b353c5a4c29828f6520a0ec6734d7c28
Mikhail Klimenok <mikhail.klimenok@gmail.com>
2025-04-06 21:48:52 +0300

    user-data-dir

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -21,22 +21,15 @@ public class DriverFactory {
         ChromeOptions options = new ChromeOptions();
 
         // Stealth mode: pretending to be a regular user. Without it tests can fail because of captcha
-/*
         options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
         options.setExperimentalOption("excludeSwitches", Arrays.asList("enable-automation"));
         options.setExperimentalOption("useAutomationExtension", false);
         options.addArguments("--disable-blink-features=AutomationControlled");
-*/
+
 
         // In CI (GitHub Actions), use a unique profile directory to avoid conflicts
-      /*  if (System.getenv("GITHUB_ACTIONS") != null) {
-            try {
-                Path tempDir = Files.createTempDirectory("chrome-profile-");
-                options.addArguments("--user-data-dir=" + tempDir.toAbsolutePath());
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to create temporary profile dir for Chrome", e);
-            }
-        }*/
+        String uniqueProfile = System.getProperty("java.io.tmpdir") + "/chrome-profile-" + UUID.randomUUID();
+        options.addArguments("--user-data-dir=" + uniqueProfile);
 
         driver.set(new ChromeDriver(options));
         driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
